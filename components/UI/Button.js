543e95b423edc167b5ca3c48147c91cf938bc34d
Chaffexd <shanechaffe@gmail.com>
2023-10-15 21:43:42 +0100

    fixed the button

--- components/UI/Button.js
+++ components/UI/Button.js
@@ -3,12 +3,12 @@ import ArrowLeft from "../Icons/ArrowLeft";
 
 const Button = () => {
   return (
-    <button className="p-4 w-36 shadow-lg rounded-md mb-16 dark:text-white dark:bg-gray-700 dark:shadow-2xl">
-      <Link href={"/"} className="flex items-center">
+    <Link href={"/"} className="block w-40">
+      <button className="flex items-center p-4 w-36 shadow-lg rounded-md mb-16 dark:text-white dark:bg-gray-700 dark:shadow-2xl">
         <ArrowLeft />
         Back
-      </Link>
-    </button>
+      </button>
+    </Link>
   );
 };
 
